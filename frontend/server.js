3cbe3eedc30323a8bf7c11c6518af1a446b8fd4b
*Erahum* <74726406+Errahum@users.noreply.github.com>
2024-10-27 23:18:27 -0400

    Refactor server.js to improve Content-Security-Policy configuration

--- frontend/server.js
+++ frontend/server.js
@@ -17,15 +17,15 @@ app.use((req, res, next) => {
   res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
-  res.setHeader('Content-Security-Policy', `
-    default-src 'self';
-    script-src 'self' https://apis.google.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://www.youtube.com https://www.gstatic.com https://www.google-analytics.com https://js.stripe.com https://www.googletagmanager.com 'sha256-${scriptHash}';
-    style-src 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com;
-    img-src 'self' data: https://www.google-analytics.com;
-    font-src 'self' https://fonts.gstatic.com;
-    frame-src 'self' https://www.youtube.com https://js.stripe.com;
-    connect-src 'self' https://api.stripe.com https://*.supabase.co https://fineurai-9hjoe.ondigitalocean.app;
-  `);
+  res.setHeader('Content-Security-Policy', 
+    `default-src 'self'; ` +
+    `script-src 'self' https://apis.google.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://www.youtube.com https://www.gstatic.com https://www.google-analytics.com https://js.stripe.com https://www.googletagmanager.com 'sha256-${scriptHash}'; ` +
+    `style-src 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com; ` +
+    `img-src 'self' data: https://www.google-analytics.com; ` +
+    `font-src 'self' https://fonts.gstatic.com; ` +
+    `frame-src 'self' https://www.youtube.com https://js.stripe.com; ` +
+    `connect-src 'self' https://api.stripe.com https://*.supabase.co https://fineurai-9hjoe.ondigitalocean.app;`
+  );
   next();
 });
 

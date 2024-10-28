fcecb05a5531bd1ea6a2639326dd06e1e135e617
*Erahum* <74726406+Errahum@users.noreply.github.com>
2024-10-27 20:35:43 -0400

    Erreur CSP
    script-src 'nonce-${nonce}' et style-src 'nonce-${nonce}' : Utilisation de nonce pour autoriser les scripts et styles inline de manière sécurisée.
    font-src 'self' https://fonts.gstatic.com : Autorisation des polices de Google Fonts.

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
+const crypto = require('crypto');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to set security headers
 app.use((req, res, next) => {
+  const nonce = crypto.randomBytes(16).toString('base64');
   res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
-  res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' https://apis.google.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://www.youtube.com https://www.gstatic.com https://www.google-analytics.com https://js.stripe.com https://www.googletagmanager.com; style-src 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com; img-src 'self' data: https://www.google-analytics.com; frame-src 'self' https://www.youtube.com https://js.stripe.com; connect-src 'self' https://api.stripe.com https://*.supabase.co https://fineurai-9hjoe.ondigitalocean.app;");
+  res.setHeader('Content-Security-Policy', `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://apis.google.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://www.youtube.com https://www.gstatic.com https://www.google-analytics.com https://js.stripe.com https://www.googletagmanager.com; style-src 'self' 'nonce-${nonce}' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com; img-src 'self' data: https://www.google-analytics.com; font-src 'self' https://fonts.gstatic.com; frame-src 'self' https://www.youtube.com https://js.stripe.com; connect-src 'self' https://api.stripe.com https://*.supabase.co https://fineurai-9hjoe.ondigitalocean.app;`);
+  res.locals.nonce = nonce; // Make nonce available to templates
   next();
 });
 

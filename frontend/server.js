40b2f50d9d85621f83fbd6f172c6540a3d4f8485
*Erahum* <74726406+Errahum@users.noreply.github.com>
2024-10-27 23:35:13 -0400

    Cloudflare et font

--- frontend/server.js
+++ frontend/server.js
@@ -12,6 +12,9 @@ const scriptHash = crypto.createHash('sha256').update(`
   gtag('config', 'G-5HKVCPG30X');
 `).digest('base64');
 
+// Calculer le hash SHA-256 du style inline
+const styleHash = 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=';
+
 // Middleware to set security headers
 app.use((req, res, next) => {
   res.setHeader('X-Frame-Options', 'SAMEORIGIN');
@@ -19,8 +22,8 @@ app.use((req, res, next) => {
   res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
   res.setHeader('Content-Security-Policy', 
     `default-src 'self'; ` +
-    `script-src 'self' https://apis.google.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://www.youtube.com https://www.gstatic.com https://www.google-analytics.com https://js.stripe.com https://www.googletagmanager.com 'sha256-${scriptHash}'; ` +
-    `style-src 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com; ` +
+    `script-src 'self' https://apis.google.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://www.youtube.com https://www.gstatic.com https://www.google-analytics.com https://js.stripe.com https://www.googletagmanager.com 'sha256-${scriptHash}' https://challenges.cloudflare.com; ` +
+    `style-src 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com 'sha256-${styleHash}'; ` +
     `img-src 'self' data: https://www.google-analytics.com; ` +
     `font-src 'self' https://fonts.gstatic.com; ` +
     `frame-src 'self' https://www.youtube.com https://js.stripe.com; ` +

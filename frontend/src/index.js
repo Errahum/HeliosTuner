3f24c75f24c385c709f1410ab4b19680154a309f
*Erahum* <74726406+Errahum@users.noreply.github.com>
2024-10-27 22:34:01 -0400

    Fix police

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,19 +21,8 @@ import HelmetPageInfo from './pageInfo'; // Importer le composant PageInfo
 import HeaderDashboard2 from './openai_product/header_dashboard2';
 import BackendStatusChecker from './backend-check/BackendStatusChecker';
 
-// Function to get nonce from meta tag
-const getNonce = () => {
-  const meta = document.querySelector('meta[name="csp-nonce"]');
-  return meta ? meta.getAttribute('content') : '';
-};
-
 // Google tag (gtag.js)
 const addGoogleTag = () => {
-  const nonce = getNonce();
-  if (!nonce) {
-    console.error('Nonce not found');
-    return;
-  }
   const script1 = document.createElement('script');
   script1.async = true;
   script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-5HKVCPG30X';
@@ -46,7 +35,6 @@ const addGoogleTag = () => {
     gtag('js', new Date());
     gtag('config', 'G-5HKVCPG30X');
   `;
-  script2.setAttribute('nonce', nonce);
   document.head.appendChild(script2);
 };
 
